f5fe6ac5fb5e6b6d00fd404fa6e400ccddc9afa8
Timur Takhtarov <timtaktamu@tamu.edu>
2025-12-02 18:15:41 +0800

    Initial Commit

---
+++ thoughts/thoughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  thoughtsApp.swift
+//  thoughts
+//
+//  Created by Timur Takhtarov on 12/2/25.
+//
+
+import SwiftUI
+
+@main
+struct thoughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
